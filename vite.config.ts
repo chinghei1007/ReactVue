f7fdad6e44e1a262d6a8115cf59f861af6e6eb8a
chinghei1007 <35035492+chinghei1007@users.noreply.github.com>
2026-07-05 14:54:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), Pages({
     onRoutesGenerated(routes) {
       console.log('Generated routes:', JSON.stringify(routes, null, 2))
     },
-  }), cloudflare()],
+  }),],
   server: {
     proxy: {
       '/api': {
